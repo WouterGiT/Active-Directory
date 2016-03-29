75daacf86c96d8362f2137dd49904c7a0d2b31f1
WouterGiT <wdedood@outlook.com>
2016-03-29 16:01:31 +0200

    Initial Start

---
+++ Add-NewADGlobalGroup.ps1
@@ -0,0 +1,88 @@
+﻿Function Add-NewADGlobalGroup {
+    <#
+    .SYNOPSIS
+        Create a new Active Directory Global Group
+ 
+    .DESCRIPTION
+        Create a new Active Directory Global Group, query the PDC as primary server
+        Active Directory CMDLets are needed.
+        The group is only created of it doesn't exists.
+ 
+    .PARAMETER Name
+        The name of the new Global Group.
+
+    .PARAMETER Path
+        Specifies the OU were to create the new Global Group.
+        "OU=New Group OU,OU=Groups,DC=prutser,DC=me"
+ 
+    .EXAMPLE
+        Add-NewADGlobalGroup -Name "Group Name" -Path "OU=New Group OU,OU=Groups,DC=prutser,DC=me"
+    
+    .EXAMPLE
+        Add-NewADGlobalGroup -Name "GroupName" -Path "OU=prutser,DC=prutser,DC=me"
+
+    .INPUTS
+        String
+ 
+    .OUTPUTS
+        Creates a new Global Group in Active Directory.
+        Status is outputted to screen
+ 
+    .NOTES
+        Author:  Wouter de Dood
+        Website: 
+        Twitter: @WMouter
+    #>
+
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [string]$Name,
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [string]$Path
+    )
+
+    Begin {
+        $ErrorActionPreference = 'Stop'
+        Try {
+            Import-Module ActiveDirectory
+        }
+        Catch {
+            Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Error] Module not loaded, ActiveDirectory Module is mandatory."
+            Throw
+        }
+        $Identity = "CN=$Name,$Path"
+        $Server   = (Get-ADDomainController -Service PrimaryDC -Discover).HostName #Target PDC of current domain
+    }
+    Process {
+        $ErrorActionPreference = 'Stop'
+        Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Action] Create group $groupName in $groupPath"
+        Try {
+            Get-ADGroup -Identity $Identity -Server $Server | Out-Null
+        }
+        Catch {
+            $ouCreate = $true
+        }
+
+        if ($ouCreate -eq $true) {
+            Try {
+                New-ADGroup -Name $Name -Path $Path -GroupScope Global -Server $Server
+                Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Status] Created"
+            }
+            Catch {
+                Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Error] Not created!"
+                Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Error] $($_.Exception.Message)"
+            }
+        } else {
+            Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Status] No need to create already exists"
+        }
+    }
+    End {
+        Remove-Variable ouCreate,Name,Path,Server,Identity
+    }
+
+} #end function Add-NewADGlobalGroup
---
+++ Add-NewOrganizationalUnit.ps1
@@ -0,0 +1,84 @@
+﻿Function Add-NewOrganizationalUnit {
+    <#
+    .SYNOPSIS
+        Create a new Organizational Unit
+ 
+    .DESCRIPTION
+        Create a new Organizational Unit, query the PDC as primary server
+        Active Directory CMDLets are needed.
+ 
+    .PARAMETER Name
+        The name of the new Organizational Unit.
+
+    .PARAMETER Path
+        Specifies the OU were to create the new Organizational Unit.
+ 
+    .EXAMPLE
+         Add-NewOrganizationalUnit -Name 'OU Name'
+    
+    .EXAMPLE
+         Add-NewOrganizationalUnit -Name 'OU Name' -Path 'OU=My Location,OU=Servers,DC=prutser,DC=me'
+
+    .INPUTS
+        String
+ 
+    .OUTPUTS
+        Creates a new Organizational Unit in Active Directory
+        Status is outputted to screen
+ 
+    .NOTES
+        Author:  Wouter de Dood
+        Website: 
+        Twitter: @WMouter
+    #>
+
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [string]$Name,
+        [Parameter(ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [string]$Path = 'OU=Customers,OU=Sales,DC=prutser,DC=me' #Set this path as default
+    )
+    Begin {
+        $ErrorActionPreference = 'Stop'
+        Try {
+            Import-Module ActiveDirectory
+        }
+        Catch {
+            Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Error] Module not loaded, ActiveDirectory Module is mandatory."
+            Throw
+        }
+        $Identity = "OU=$Name,$Path"
+        $Server   = (Get-ADDomainController -Service PrimaryDC -Discover).HostName #Target PDC of current domain
+    }
+    Process {
+        $ErrorActionPreference = 'Stop'
+        Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Action] Create Organizational Unit: $Name in $Path"
+        Try {
+            Get-ADOrganizationalUnit -Identity $Identity -Server $Server | Out-Null
+        }
+        Catch {
+            $ouCreate = $true
+        }
+
+        if ($ouCreate -eq $true) {
+            Try {
+                New-ADOrganizationalUnit -Name $Name -Path $Path -Server $Server
+                Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Status] Created"
+            }
+            Catch {
+                Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Error] Not created!"
+                Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Error] $($_.Exception.Message)"
+            }
+        } else {
+            Write-Output "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S")] - [Status] No need to create already exists"
+        }
+    }
+    End {
+        Remove-Variable ouCreate,Name,Path,Server,Identity
+    }
+
+} #end function Add-NewOrganizationalUnit
